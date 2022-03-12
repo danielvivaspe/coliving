137cd07697cddf2b8891b6d05890e520ac3ff0b2
David Guix <davidguixsanchez83@gmail.com>
2022-03-12 23:52:28 +0100

    Update Recommender.py

--- Recommender.py
+++ Recommender.py
@@ -10,7 +10,7 @@ class Recommender:
     def get_prediction(self, userID, activityID):
         return self.model.predict(userID, activityID)
 
-    def get_recommendations(self, userId, dataframe, algorithm, n_recommendations, column_iid= None, column_uid= None):
+    def get_recommendations(self, userId, dataframe, n_recommendations, column_iid= None, column_uid= None):
         """
         This functions will use a trained algorithm to find the n top list of recommended items for a given userID.
 
@@ -20,8 +20,6 @@ class Recommender:
         userId (int): the user ID of the person that we want recommendations for.
 
         dataframe (object): the DataFrame containing three columns; userID, itemID and rating.
-
-        algorithm (object): the trained algorith used to recommend items.
 
         n_rcommendations (int): the number of items recommended.
 
@@ -46,7 +44,7 @@ class Recommender:
 
         preds = []
         for item in items_no_finished:
-            preds.append(SVD_model_for_pickle.predict(uid=userId, iid=item))
+            preds.append(self.model.predict(uid=userId, iid=item))
 
         recommendations_rating = {pred[1]:pred[3] for pred in preds}
 
@@ -55,3 +53,29 @@ class Recommender:
         top_predictions = list(order_dict.keys())[:n_recommendations]
         
         return top_predictions
+        
+    def check_activities_user(userId, dataframe, n, column_rating= None, column_uid= None):
+        """
+        This functions will show the n top rated items for a given userID.
+
+        Parameters
+        -----------
+
+        userId (int): the user ID of the person that we want recommendations for.
+
+        dataframe (object): the DataFrame containing three columns; userID, itemID and rating.
+
+        n (int): number of top rated items to show.
+
+        column_rating (string): name of the column containing the item rating.
+
+        column_uid (string): name of the column containing the user ID.
+
+
+        return
+        ------
+
+        A dataframe with the n top rated items by that given user.
+
+        """
+        return dataframe[dataframe[column_uid] ==userId].sort_values(column_rating, ascending=False)[:n]
